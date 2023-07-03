b15454026610d36429c5dd59ed522a06de623d5f
Vaibhav <vaibhavmahajan3003@gmail.com>
2023-07-03 14:39:37 +0530

    Single Product

---
+++ src/components/ProductImages.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const ProductImages = ({images}) => {
+    const [mainImage,setMainImage] = useState(images[0]);
+  return (
+    <div className='w-11/12 mx-auto'>
+        <div className='w-full'>
+            <img src={mainImage.url} alt={images[0].fileName} />
+        </div>
+      <div className='flex justify-center items-center w-full py-4'>
+        {
+            images.map((e,i)=>{
+                return (
+                    <figure>
+                        <img src={e.url} alt={e.fileName} key={i} className='w-16 h-10 mr-4 hover:cursor-pointer' 
+                        onClick={()=>setMainImage(e)}/>
+                    </figure>
+                )
+            })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default ProductImages
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -3,6 +3,8 @@ import { useEffect,useContext } from 'react';
 import { useParams,Link } from 'react-router-dom'
 import { AppContext } from '../context/ProductContext';
 import FormatPrice from '../helper/FormatPrice';
+import {TbTruckDelivery} from 'react-icons/tb'
+import ProductImages from '../components/ProductImages';
 
 const API = "https://api.pujakaitem.com/api/products";
 
@@ -33,42 +35,85 @@ const SingleProduct = () => {
       <div className="text-md breadcrumbs mb-10 px-5">
         <ul>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to="/">Home</Link>
           </li>
-          <li>
-            {name}
-          </li>
+          <li>{name}</li>
         </ul>
       </div>
 
-      {isSingleLoading && <h1 className='text-lg text-center'>Loading...</h1>}
+      {isSingleLoading && <h1 className="text-lg text-center">Loading...</h1>}
 
-      <div className='border mb-10 w-full'>
-        <div className='w-11/12 lg:w-4/5 flex flex-col border md:flex-row mx-auto'>
-          <div className='w-full md:w-1/2'>
-            Images
+      <div className="mb-10 w-full">
+        <div className="w-11/12 lg:w-4/5 flex flex-col md:flex-row mx-auto">
+          <div className="w-full md:w-1/2 flex justify-center items-center">
+            <ProductImages images={image}/>
           </div>
-          <div className='border py-2 w-full md:w-1/2'>
-            <div className='w-full mb-2'>
-              <h2 className='text-3xl font-bold'>{name}</h2>
+          <div className="p-2 w-full md:w-1/2">
+            <div className="w-full mb-4 flex justify center items-center">
+              <h2 className="text-3xl font-bold">{name}</h2>
+              <span className="badge ml-2">{category}</span>
             </div>
-            <div className='w-full'>
-              <p className='mb-2'>{stars}</p>
-              <p className='mb-2'>{reviews}</p>
-              <p className='mb-2'>
-                MRP:
+            <div className="w-full">
+              <p className="mb-4">{stars}</p>
+              <p className="mb-4">{reviews} reviews</p>
+              <p className="mb-4 font-semibold">
+                MRP :
                 <del>
-                  <FormatPrice price={price + 250000}/>
-                </del> 
+                  <FormatPrice price={price + 250000} />
+                </del>
               </p>
+              <p className="font-semibold text-orange-400 mb-4">
+                Deal of the Day : <FormatPrice price={price} />
+              </p>
+              <p className="text-justify mb-4">{description}</p>
+
+              <div className="w-full flex justify-between mb-4 pt-2 border-b border-gray-800">
+                <div className="flex flex-col justify-center items-center">
+                  <div className="p-2 w-fit rounded-full bg-gray-200">
+                    <TbTruckDelivery className="text-lg font-bold" />
+                  </div>
+                  <p className="text-sm mt-1">Free Delievery</p>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                  <div className="p-2 w-fit rounded-full bg-gray-200">
+                    <TbTruckDelivery className="text-lg font-bold" />
+                  </div>
+                  <p className="text-sm mt-1">30 days of replacement</p>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                  <div className="p-2 w-fit rounded-full bg-gray-200">
+                    <TbTruckDelivery className="text-lg font-bold" />
+                  </div>
+                  <p className="text-sm mt-1">Tech Delievered</p>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                  <div className="p-2 w-fit rounded-full bg-gray-200">
+                    <TbTruckDelivery className="text-lg font-bold" />
+                  </div>
+                  <p className="text-sm mt-1">2 Year Warranty</p>
+                </div>
+              </div>
+
+              <p className="mb-4">
+                Available :{" "}
+                {stock > 0 ? (
+                  <span className="text-md font-semibold text-green-500">
+                    In Stock
+                  </span>
+                ) : (
+                  <span className="text-md font-semibold text-red-500">
+                    Not Available
+                  </span>
+                )}
+              </p>
+
               <p>
-                Deal of the Day : <FormatPrice price={price}/>
+                Brand : <span className="font-semibold">{company}</span>
               </p>
             </div>
           </div>
         </div>
       </div>
-      
     </div>
   );
 }
