dd73790f070973d3c6b1a2c5641db54ac25f505d
Vaibhav <vaibhavmahajan3003@gmail.com>
2023-07-10 15:08:40 +0530

    Cart Item

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
+import FormatPrice from '../helper/FormatPrice'
 
-const CartItem = () => {
+const CartItem = ({data}) => {
     const {cart} = useContext(CartContext);
     console.log(cart);
   return (
+    
     <div className="py-8 flex border-t">
-      <div className="w-fit flex justify-center items-center rounded-lg">
+      <div className="w-fit flex justify-center items-center rounded-lg shadow-lg">
         <img
-          src="./images/logo.png"
+          src={data.image}
           alt=""
           className="w-48 rounded-lg bg-cover"
         />
@@ -18,16 +20,19 @@ const CartItem = () => {
         <div className="flex justify-between w-full flex-wrap">
           <div className="flex flex-col gap-2">
             <h3 className="text-lg">
-              <a href="#">{cart["name"]}</a>
+              <a href="#" className='capitalize font-semibold'>{data.name}</a>
             </h3>
-            <p className="text-md">Color</p>
+            <p className="text-md w-5 h-5 rounded-full" style={{backgroundColor:data.color}}></p>
           </div>
           <div className="flex flex-col gap-2">
-            <p className="text-lg">Quantity</p>
-            <p>Remove</p>
+            <p className="text-lg">{data.amount}</p>
+            {/* <select name="" id="">
+              
+            </select> */}
+            <button className='text-red-500'>Remove</button>
           </div>
           <div>
-            <p className="text-lg">Price</p>
+            <FormatPrice price={data.price}/>
           </div>
         </div>
       </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,19 @@
 import React, { useContext } from 'react'
 import CartItem from '../components/CartItem'
+import { CartContext } from '../context/CartContext'
 
 const Cart = () => {
+  const {cart} = useContext(CartContext)
   return (
     <div className='py-20 border'>
       <div className='w-11/12 md:w-4/5 mx-auto py-6 flex flex-col gap-4'>
         <h2 className='text-xl md:text-2xl lg:text-4xl font-bold py-4'>Shopping Cart</h2>
-        <CartItem />
+        {
+          cart.map((data)=>{
+            return <CartItem data={data}/>
+          })
+        }
+        
       </div>
     </div>
   )
